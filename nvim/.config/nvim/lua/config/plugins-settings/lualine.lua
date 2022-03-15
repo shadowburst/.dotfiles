7ac41701e3f20b44938c53d11615fae96bf40428
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2022-03-15 21:43:40 +0100

    Nvim - Format some plugins

--- nvim/.config/nvim/lua/config/plugins-settings/lualine.lua
+++ nvim/.config/nvim/lua/config/plugins-settings/lualine.lua
@@ -1,4 +1,4 @@
-local status_ok, lualine = pcall(require, "lualine")
+local status_ok, lualine = pcall(require, 'lualine')
 if not status_ok then
 	return
 end
@@ -8,44 +8,44 @@ local hide_in_width = function()
 end
 
 local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
+	'diagnostics',
+	sources = { 'nvim_diagnostic' },
+	sections = { 'error', 'warn' },
+	symbols = { error = ' ', warn = ' ' },
 	colored = false,
 	update_in_insert = false,
 	always_visible = true,
 }
 
 local diff = {
-	"diff",
+	'diff',
 	colored = false,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
+	symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+	cond = hide_in_width,
 }
 
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		theme = 'auto',
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = { 'alpha', 'dashboard', 'NvimTree', 'Outline' },
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch" },
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', diff },
 		lualine_c = { diagnostics },
-		lualine_x = { diff, "encoding", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
+		lualine_x = { 'encoding' },
+		lualine_y = { 'filetype' },
+		lualine_z = { 'location' },
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
 		lualine_y = {},
 		lualine_z = {},
 	},
