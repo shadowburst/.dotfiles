eb3a2f2faf03a2e511128e5605b06e2a1f7bc886
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2022-04-03 13:10:25 +0200

    Awesome - Set firefox as main browser

--- awesome/.config/awesome/configuration/apps.lua
+++ awesome/.config/awesome/configuration/apps.lua
@@ -1,7 +1,7 @@
 local terminal = 'kitty'
 local apps = {
 	terminal = terminal,
-	browser = 'brave',
+	browser = 'firefox',
 	editor = terminal .. ' --class nvim,nvim -e nvim',
 	file_manager = terminal .. ' --class ranger,ranger -e ranger',
 }
