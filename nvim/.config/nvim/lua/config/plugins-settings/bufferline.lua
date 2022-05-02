0ab5ba0e8eb2f8c43770ad498e9b6f5e4f1b564c
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2022-05-02 07:59:27 +0200

    Nvim - Fix BufferLine close

--- nvim/.config/nvim/lua/config/plugins-settings/bufferline.lua
+++ nvim/.config/nvim/lua/config/plugins-settings/bufferline.lua
@@ -6,8 +6,8 @@ end
 bufferline.setup({
 	options = {
 		numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-		close_command = 'BDelete! %d', -- can be a string | function, see "Mouse actions"
-		right_mouse_command = 'BDelete! %d', -- can be a string | function, see "Mouse actions"
+		close_command = 'lua require("close_buffers").delete({type = %d})', -- can be a string | function, see "Mouse actions"
+		right_mouse_command = 'lua require("close_buffers").delete({type = %d})', -- can be a string | function, see "Mouse actions"
 		left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
 		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
 		-- NOTE: this plugin is designed with this icon in mind,
