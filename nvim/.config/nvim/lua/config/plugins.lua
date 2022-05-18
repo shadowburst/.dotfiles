566c0d6fa42e98083fa577f907a986d3e17f8d53
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2022-05-18 23:08:05 +0200

    Nvim - Add gitignore generation

--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -44,8 +44,6 @@ packer.startup(function(use)
 	use('wbthomason/packer.nvim') -- Have packer manage itself
 	use('nvim-lua/popup.nvim') -- An implementation of the Popup API from vim in Neovim
 	use('nvim-lua/plenary.nvim') -- Useful lua functions used ny lots of plugins
-	use('kazhala/close-buffers.nvim')
-	use('lewis6991/impatient.nvim')
 
 	-- UI
 	use('folke/which-key.nvim')
@@ -95,6 +93,11 @@ packer.startup(function(use)
 	-- snippets
 	use('L3MON4D3/LuaSnip') --snippet engine
 	use('rafamadriz/friendly-snippets') -- a bunch of snippets to use
+
+	-- other
+	use('kazhala/close-buffers.nvim')
+	use('lewis6991/impatient.nvim')
+	use('kalvinpearce/gitignore-gen.nvim')
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
