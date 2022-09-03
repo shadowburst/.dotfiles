bfb553aea5bf44fc6ba30f1521d6f5af23b273a0
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2022-09-03 13:12:11 +0200

    Fish - Fix extract function

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -33,25 +33,25 @@ alias emacsterm='emacsclient -t'
 function extract
     if test -f $argv
         switch $argv
-            case *.tar.bz2
+            case "*.tar.bz2"
                 tar xjf $argv
-            case *.tar.gz
+            case "*.tar.gz"
                 tar xzf $argv
-            case *.bz2
+            case "*.bz2"
                 bunzip2 $argv
-            case *.rar
+            case "*.rar"
                 rar x $argv
-            case *.gz
+            case "*.gz"
                 gunzip $argv
-            case *.tar
+            case "*.tar"
                 tar xf $argv
-            case *.tbz2
+            case "*.tbz2"
                 tar xjf $argv
-            case *.tgz
+            case "*.tgz"
                 tar xzf $argv
-            case *.zip
+            case "*.zip"
                 unzip $argv
-            case *.Z
+            case "*.Z"
                 uncompress $argv
             case "*"
                 echo "'$argv' cannot be extracted via extract()"
