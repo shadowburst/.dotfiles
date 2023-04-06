c5b83d72607e17cefde99e0e716d90dc65f16ce0
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-04-06 20:55:44 +0200

    Change neotree untracked color

--- editors/.config/nvim/lua/plugins/editor.lua
+++ editors/.config/nvim/lua/plugins/editor.lua
@@ -26,41 +26,54 @@ return {
 		keys = {
 			{ "<leader>E", false },
 		},
-		opts = {
-			filesystem = {
-				bind_to_cwd = true,
-				follow_current_file = true,
-				group_empty_dirs = true,
-				cwd_target = {
-					sidebar = "window",
-				},
-				filtered_items = {
-					visible = true,
-					hide_dotfiles = false,
-					-- hide_gitignored = false,
-					hide_hidden = false,
-					hide_by_name = {
-						".git",
-						"node_modules",
-						"vendor",
+		opts = function()
+			vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { italic = true, fg = "#98be65" })
+
+			return {
+				filesystem = {
+					bind_to_cwd = true,
+					follow_current_file = true,
+					group_empty_dirs = true,
+					cwd_target = {
+						sidebar = "window",
+					},
+					filtered_items = {
+						visible = true,
+						hide_dotfiles = false,
+						-- hide_gitignored = false,
+						hide_hidden = false,
+						hide_by_name = {
+							".git",
+							"node_modules",
+							"vendor",
+						},
 					},
 				},
-			},
-			window = {
-				mappings = {
-					["h"] = "close_node",
-					["l"] = "open",
+				window = {
+					mappings = {
+						["<space>"] = "none",
+						["h"] = "close_node",
+						["l"] = "open",
+					},
 				},
-			},
-			event_handlers = {
-				{
-					event = "file_opened",
-					handler = function()
-						require("neo-tree").close_all()
-					end,
+				default_component_configs = {
+					indent = {
+						with_expanders = true,
+						expander_collapsed = "",
+						expander_expanded = "",
+						expander_highlight = "NeoTreeExpander",
+					},
 				},
-			},
-		},
+				event_handlers = {
+					{
+						event = "file_opened",
+						handler = function()
+							require("neo-tree").close_all()
+						end,
+					},
+				},
+			}
+		end,
 	},
 	{
 		"lambdalisue/suda.vim",
