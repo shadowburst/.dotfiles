8be38f389369e76dae76656f0743372f9f63a475
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-04-06 21:30:26 +0200

    Fix nvim autocmd in non-file buffers

--- editors/.config/nvim/lua/config/autocmds.lua
+++ editors/.config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,10 @@
 -- Fix folding when opening files with telescope
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	pattern = { "*" },
-	command = "normal zx",
+	callback = function()
+		-- Only execute in files
+		if vim.bo.buftype == "" and vim.bo.filetype ~= "" then
+			vim.cmd.normal("zx")
+		end
+	end,
 })
