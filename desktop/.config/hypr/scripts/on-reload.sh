bc12877efa9abb39bfdbeee102eac1d9aac1ebb2
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-04-28 22:41:44 +0200

    Format some scripts and update packages

--- desktop/.config/hypr/scripts/on-reload.sh
+++ desktop/.config/hypr/scripts/on-reload.sh
@@ -4,7 +4,6 @@ pkill swaybg
 swaybg -i ~/.wallpapers/current.jpg -m fill &
 
 eww close-all
-hyprctl monitors -j | jq --raw-output .[].id | while read -r id
-do
-  eww open "bar-$id"
+hyprctl monitors -j | jq --raw-output .[].id | while read -r id; do
+	eww open "bar-$id"
 done
