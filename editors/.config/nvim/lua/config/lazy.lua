7bad021e91395f86411bef568aea3e3e7e470e29
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-07-14 00:44:50 +0200

    feat(nvim): update plugins

--- editors/.config/nvim/lua/config/lazy.lua
+++ editors/.config/nvim/lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
 		},
 
 		-- import any extras modules here
-		{ import = "lazyvim.plugins.extras.editor.flash" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
