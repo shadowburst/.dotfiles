34511e6c00c6a93fc53d817d1e14364ea8f531e3
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-08-28 16:28:47 +0200

    feat(nvim): move keymaps to corresponding plugins

--- editors/.config/nvim/lua/plugins/harpoon.lua
+++ editors/.config/nvim/lua/plugins/harpoon.lua
@@ -3,42 +3,56 @@ return {
 		"ThePrimeagen/harpoon",
 		keys = {
 			{
-				"<leader>a",
+				"<S-h>",
+				function()
+					require("harpoon.ui").nav_prev()
+				end,
+				desc = "Go to previous marked file",
+			},
+			{
+				"<S-l>",
+				function()
+					require("harpoon.ui").nav_next()
+				end,
+				desc = "Go to next marked file",
+			},
+			{
+				"<leader>ma",
 				function()
 					require("harpoon.mark").add_file()
 				end,
 				desc = "Add current file to harpoon",
 			},
 			{
-				"<M-à>",
+				"<leader>mm",
 				function()
 					require("harpoon.ui").toggle_quick_menu()
 				end,
 				desc = "Toggle harpoon menu",
 			},
 			{
-				"<M-&>",
+				"<leader>mh",
 				function()
 					require("harpoon.ui").nav_file(1)
 				end,
 				desc = "Goto file 1",
 			},
 			{
-				"<M-é>",
+				"<leader>mj",
 				function()
 					require("harpoon.ui").nav_file(2)
 				end,
 				desc = "Goto file 2",
 			},
 			{
-				'<M-">',
+				"<leader>mk",
 				function()
 					require("harpoon.ui").nav_file(3)
 				end,
 				desc = "Goto file 3",
 			},
 			{
-				"<M-'>",
+				"<leader>ml",
 				function()
 					require("harpoon.ui").nav_file(4)
 				end,
