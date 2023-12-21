46a2ea590b02632291886f82e7181da7ce470d60
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2023-12-21 16:15:20 +0100

    feat(nvim): add dotfiles treesitter

--- terminal/.config/nvim/lua/config/lazy.lua
+++ terminal/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.ui.edgy" },
+		{ import = "lazyvim.plugins.extras.util.dot" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
 		-- import/override with your plugins
