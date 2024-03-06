042c945d25d57965e04c49dc92574eec6512960b
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-03-06 13:59:00 +0100

    fix(nvim): hide tabline

--- terminal/.config/nvim/lua/config/options.lua
+++ terminal/.config/nvim/lua/config/options.lua
@@ -28,6 +28,7 @@ opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
 opt.showmode = false -- Dont show mode since we have a statusline
+opt.showtabline = 0
 opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
