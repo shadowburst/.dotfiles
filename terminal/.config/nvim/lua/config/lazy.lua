23e6d70fdd383455d43b625efdd77f0bd557ce1a
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-03-06 07:29:58 +0100

    feat(nvim): add codium & dap

--- terminal/.config/nvim/lua/config/lazy.lua
+++ terminal/.config/nvim/lua/config/lazy.lua
@@ -21,6 +21,8 @@ require("lazy").setup({
 		},
 
 		-- import any extras modules here
+		{ import = "lazyvim.plugins.extras.coding.codeium" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
