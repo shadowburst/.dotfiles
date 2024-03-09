4c4facd5db79c05a0b86443917a609425f524179
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-03-09 23:23:23 +0100

    feat(nvim): use better keymaps

--- terminal/.config/nvim/lua/plugins/nvim-cmp.lua
+++ terminal/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,18 +3,39 @@ return {
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"hrsh7th/cmp-cmdline",
-			{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+			{ "roobert/tailwindcss-colorizer-cmp.nvim", opts = {} },
 		},
 		opts = function(_, opts)
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
-
 			local luasnip = require("luasnip")
 			local cmp = require("cmp")
+
+			opts.mapping = vim.tbl_extend("force", opts.mapping, {
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete({}),
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					end
+				end, { "i", "s" }),
+				["<C-h>"] = cmp.mapping(function()
+					if luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					end
+				end, { "i", "s" }),
+			})
+
+			opts.window = {
+				completion = {
+					border = "rounded",
+					winhighlight = "Normal:Pmenu",
+				},
+				documentation = {
+					border = "rounded",
+					winhighlight = "Normal:Pmenu",
+				},
+			}
 
 			cmp.setup.cmdline("/", {
 				mapping = cmp.mapping.preset.cmdline(),
@@ -36,40 +57,6 @@ return {
 					},
 				}),
 			})
-
-			opts.mapping = vim.tbl_extend("force", opts.mapping, {
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			})
-
-			opts.window = {
-				completion = {
-					border = "rounded",
-					winhighlight = "Normal:Pmenu",
-				},
-				documentation = {
-					border = "rounded",
-					winhighlight = "Normal:Pmenu",
-				},
-			}
 		end,
 	},
 }
