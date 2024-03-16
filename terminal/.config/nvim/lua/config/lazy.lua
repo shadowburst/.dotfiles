64148aa5dca68d19f62b33966367a15d51bd0325
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-03-16 18:57:38 +0100

    build(nvim): remove dap and update plugins

--- terminal/.config/nvim/lua/config/lazy.lua
+++ terminal/.config/nvim/lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
 
 		-- import any extras modules here
 		{ import = "lazyvim.plugins.extras.coding.codeium" },
-		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
