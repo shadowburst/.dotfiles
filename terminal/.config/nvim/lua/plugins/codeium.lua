c890c8816c5daabf023342cc37dfc2ac9bc7c222
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-04-09 13:29:49 +0200

    fix(nvim): better codeium config

--- terminal/.config/nvim/lua/plugins/codeium.lua
+++ terminal/.config/nvim/lua/plugins/codeium.lua
@@ -1,16 +1,6 @@
 return {
 	{
 		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		keys = {
-			{
-				"<TAB>",
-				mode = "i",
-				function()
-					vim.fn["codeium#Accept"]()
-				end,
-				desc = "Accept codeium suggestion",
-			},
-		},
+		lazy = false,
 	},
 }
