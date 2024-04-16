b7dddfde8456150b5ae39f11178da2ea7d38be69
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-04-16 21:45:11 +0200

    refactor(nvim): lualine cleanup

--- terminal/.config/nvim/lua/plugins/lualine.lua
+++ terminal/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return {
 			},
 		},
 		event = "VeryLazy",
-		opts = function(_, opts)
+		opts = function()
 			local theme = require("tokyonight.colors").moon()
 
 			local colors = {
@@ -32,166 +32,131 @@ return {
 				hide_in_width = function()
 					return vim.fn.winwidth(0) > 80
 				end,
-				check_git_workspace = function()
-					local filepath = vim.fn.expand("%:p:h")
-					local gitdir = vim.fn.finddir(".git", filepath .. ";")
-					return gitdir and #gitdir > 0 and #gitdir < #filepath
-				end,
-			}
-			opts.options = opts.options or {}
-
-			opts.options.theme = "tokyonight"
-			opts.options.component_separators = ""
-			opts.options.section_separators = ""
-			opts.options.disabled_filetypes = {
-				statusline = { "dashboard", "lazy" },
 			}
 
-			opts.sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
+			return {
+				options = {
+					theme = "tokyonight",
+					component_separators = "",
+					section_separators = "",
+					disabled_filetypes = {
+						statusline = { "dashboard", "lazy" },
+					},
+				},
+				sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {
+						{
+							function()
+								return "▊"
+							end,
+							color = { fg = colors.blue },
+							padding = { left = 0, right = 1 },
+						},
+						{
+							"mode",
+							color = function()
+								local mode_color = {
+									n = colors.green,
+									i = colors.blue,
+									v = colors.yellow,
+									[""] = colors.yellow,
+									V = colors.yellow,
+									c = colors.magenta,
+									no = colors.red,
+									s = colors.orange,
+									S = colors.orange,
+									[""] = colors.orange,
+									ic = colors.yellow,
+									R = colors.magenta,
+									Rv = colors.magenta,
+									cv = colors.red,
+									ce = colors.red,
+									r = colors.cyan,
+									rm = colors.cyan,
+									["r?"] = colors.cyan,
+									["!"] = colors.red,
+									t = colors.red,
+								}
+								return { fg = colors.bg, bg = mode_color[vim.fn.mode()], gui = "bold" }
+							end,
+							separator = {
+								left = "",
+								right = "",
+							},
+						},
+						{
+							"filename",
+							cond = conditions.buffer_not_empty,
+							color = function()
+								return vim.bo.modified and { fg = colors.red, gui = "bold" }
+									or { fg = colors.fg, gui = "bold" }
+							end,
+							symbols = {
+								modified = "",
+								readonly = "",
+							},
+							padding = 2,
+						},
+						{
+							"diagnostics",
+							sources = { "nvim_diagnostic" },
+							symbols = { error = " ", warn = " ", info = " " },
+							diagnostics_color = {
+								color_error = { fg = colors.red },
+								color_warn = { fg = colors.yellow },
+								color_info = { fg = colors.cyan },
+							},
+						},
+					},
+					lualine_x = {
+						{
+							"macro-recording",
+							fmt = function()
+								local recording_register = vim.fn.reg_recording()
+								if recording_register == "" then
+									return ""
+								else
+									return "Recording @" .. recording_register
+								end
+							end,
+						},
+						{
+							"branch",
+							icon = "",
+							color = { fg = colors.magenta, gui = "bold" },
+						},
+						{
+							"diff",
+							symbols = { added = " ", modified = "柳 ", removed = " " },
+							diff_color = {
+								added = { fg = colors.green },
+								modified = { fg = colors.orange },
+								removed = { fg = colors.red },
+							},
+							cond = conditions.hide_in_width,
+						},
+						{
+							function()
+								return "▊"
+							end,
+							color = { fg = colors.blue },
+							padding = { left = 1 },
+						},
+					},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
 			}
-			opts.inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			}
-			--
-			-- Inserts a component in lualine_c at left section
-			local function ins_left(component)
-				table.insert(opts.sections.lualine_c, component)
-			end
-
-			-- Inserts a component in lualine_x ot right section
-			local function ins_right(component)
-				table.insert(opts.sections.lualine_x, component)
-			end
-
-			ins_left({
-				function()
-					return "▊"
-				end,
-				color = { fg = colors.blue }, -- Sets highlighting of component
-				padding = { left = 0, right = 1 }, -- We don't need space before this
-			})
-
-			ins_left({
-				-- mode component
-				"mode",
-				color = function()
-					-- auto change color according to neovims mode
-					local mode_color = {
-						n = colors.green,
-						i = colors.blue,
-						v = colors.yellow,
-						[""] = colors.yellow,
-						V = colors.yellow,
-						c = colors.magenta,
-						no = colors.red,
-						s = colors.orange,
-						S = colors.orange,
-						[""] = colors.orange,
-						ic = colors.yellow,
-						R = colors.magenta,
-						Rv = colors.magenta,
-						cv = colors.red,
-						ce = colors.red,
-						r = colors.cyan,
-						rm = colors.cyan,
-						["r?"] = colors.cyan,
-						["!"] = colors.red,
-						t = colors.red,
-					}
-					return { fg = colors.bg, bg = mode_color[vim.fn.mode()], gui = "bold" }
-				end,
-				separator = {
-					left = "",
-					right = "",
-				},
-			})
-
-			ins_left({
-				"filename",
-				cond = conditions.buffer_not_empty,
-				color = function()
-					return vim.bo.modified and { fg = colors.red, gui = "bold" } or { fg = colors.fg, gui = "bold" }
-				end,
-				symbols = {
-					modified = "",
-					readonly = "",
-				},
-				padding = 2,
-			})
-
-			ins_left({
-				"diagnostics",
-				sources = { "nvim_diagnostic" },
-				symbols = { error = " ", warn = " ", info = " " },
-				diagnostics_color = {
-					color_error = { fg = colors.red },
-					color_warn = { fg = colors.yellow },
-					color_info = { fg = colors.cyan },
-				},
-			})
-
-			-- Insert mid section
-			ins_left({
-				function()
-					return "%="
-				end,
-			})
-
-			-- Add components to right sections
-			ins_right({
-				"macro-recording",
-				fmt = function()
-					local recording_register = vim.fn.reg_recording()
-					if recording_register == "" then
-						return ""
-					else
-						return "Recording @" .. recording_register
-					end
-				end,
-			})
-
-			ins_right({
-				function()
-					return "%S"
-				end,
-			})
-
-			ins_right({
-				"branch",
-				icon = "",
-				color = { fg = colors.magenta, gui = "bold" },
-			})
-
-			ins_right({
-				"diff",
-				-- Is it me or the symbol for modified us really weird
-				symbols = { added = " ", modified = "柳 ", removed = " " },
-				diff_color = {
-					added = { fg = colors.green },
-					modified = { fg = colors.orange },
-					removed = { fg = colors.red },
-				},
-				cond = conditions.hide_in_width,
-			})
-
-			ins_right({
-				function()
-					return "▊"
-				end,
-				color = { fg = colors.blue },
-				padding = { left = 1 },
-			})
 		end,
 	},
 }
