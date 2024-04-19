984a2bddd679179edda75daa3db7fbdaf8aaf78b
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-04-19 21:35:34 +0200

    style(fish): indent

--- terminal/.config/fish/config.fish
+++ terminal/.config/fish/config.fish
@@ -7,11 +7,11 @@ set fish_color_option cyan
 set fish_color_error red
 set -U fish_user_paths ~/.local/bin $fish_user_paths
 if test -d ~/go/bin
-    set -U fish_user_paths ~/go/bin $fish_user_paths
+  set -U fish_user_paths ~/go/bin $fish_user_paths
 end
 
 function fish_user_key_bindings
-    fish_vi_key_bindings
+  fish_vi_key_bindings
 end
 
 # Always color output
@@ -34,34 +34,34 @@ alias mkdir='mkdir -p'
 
 # Extract all files based on type
 function extract
-    if test -f $argv
-        switch $argv
-            case "*.tar.bz2"
-                tar xjf $argv
-            case "*.tar.gz"
-                tar xzf $argv
-            case "*.bz2"
-                bunzip2 $argv
-            case "*.rar"
-                rar x $argv
-            case "*.gz"
-                gunzip $argv
-            case "*.tar"
-                tar xf $argv
-            case "*.tbz2"
-                tar xjf $argv
-            case "*.tgz"
-                tar xzf $argv
-            case "*.zip"
-                unzip $argv
-            case "*.Z"
-                uncompress $argv
-            case "*"
-                echo "'$argv' cannot be extracted via extract()"
-        end
-    else
-        echo "'$argv' is not a valid file"
+  if test -f $argv
+    switch $argv
+    case "*.tar.bz2"
+      tar xjf $argv
+    case "*.tar.gz"
+      tar xzf $argv
+    case "*.bz2"
+      bunzip2 $argv
+    case "*.rar"
+      rar x $argv
+    case "*.gz"
+      gunzip $argv
+    case "*.tar"
+      tar xf $argv
+    case "*.tbz2"
+      tar xjf $argv
+    case "*.tgz"
+      tar xzf $argv
+    case "*.zip"
+      unzip $argv
+    case "*.Z"
+      uncompress $argv
+    case "*"
+      echo "'$argv' cannot be extracted via extract()"
     end
+  else
+    echo "'$argv' is not a valid file"
+  end
 end
 
 fzf --fish | source
