a3a5cd71764e82f04a53f4120b94371af27d4a02
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-05-11 12:17:00 +0200

    feat(nvim): replace yazi with mini-files

---
+++ terminal/.config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"echasnovski/mini.files",
+		opts = {
+			mappings = {
+				go_in_plus = "l",
+			},
+			options = {
+				permanent_delete = false,
+			},
+			windows = {
+				max_number = 3,
+				preview = true,
+				width_focus = 60,
+				width_nofocus = 30,
+				width_preview = 60,
+			},
+		},
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+				end,
+				desc = "Open file explorer",
+			},
+		},
+	},
+}
