e4d6c21b6cf232a4cda134bbf54d885e2cc51891
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-05-16 19:25:26 +0200

    feat(nvim): change cmp keymaps

--- terminal/.config/nvim/lua/plugins/cmp.lua
+++ terminal/.config/nvim/lua/plugins/cmp.lua
@@ -43,12 +43,12 @@ return {
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 					["<C-Space>"] = cmp.mapping.complete({}),
-					["<C-l>"] = cmp.mapping(function()
+					["<C-k>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
 						end
 					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
+					["<C-j>"] = cmp.mapping(function()
 						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
 						end
