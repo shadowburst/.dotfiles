bf9540bc339f81eb7fbc1b232e0200eade1e1fd1
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-05-24 20:33:55 +0200

    feat(nvim): add open file mappings to tfm

--- terminal/.config/nvim/lua/plugins/tfm.lua
+++ terminal/.config/nvim/lua/plugins/tfm.lua
@@ -6,6 +6,9 @@ return {
 			replace_netrw = true,
 			keybindings = {
 				["<esc><esc>"] = "<esc>",
+				["<C-v>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.vsplit)<CR>l",
+				["<C-x>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.split)<CR>l",
+				["<C-t>"] = "<C-\\><C-O>:lua require('tfm').set_next_open_mode(require('tfm').OPEN_MODE.tabedit)<CR>l",
 			},
 			ui = {
 				border = "none",
