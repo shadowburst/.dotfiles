1f9a1da56161830d77839803b22cdd781fda813a
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-07-04 22:48:15 +0200

    feat(ags)!: replace wlogout with ags

--- desktop/.config/ags/modules/windows/index.js
+++ desktop/.config/ags/modules/windows/index.js
@@ -1,5 +1,11 @@
 import Backdrop from './backdrop.js';
+import Power from './power.js';
 import Torrents from './torrents.js';
+
+/**
+ * @typedef WindowName
+ * @type {'power'|'torrents'}
+ */
 
 export function closeAll() {
     for (const win of App.windows.filter((win) => !win.name?.startsWith('bar-'))) {
@@ -7,7 +13,7 @@ export function closeAll() {
     }
 }
 
-export function open(/** @type {'torrents'} */ name) {
+export function open(/** @type {WindowName} */ name) {
     if (App.windows.some((win) => win.name === name)) {
         return;
     }
@@ -16,8 +22,19 @@ export function open(/** @type {'torrents'} */ name) {
     App.addWindow(Backdrop());
 
     switch (name) {
+        case 'power':
+            App.addWindow(Power());
+            break;
         case 'torrents':
             App.addWindow(Torrents());
             break;
+    }
+}
+
+export function toggle(/** @type {WindowName} */ name) {
+    if (App.windows.some((win) => win.name === name)) {
+        closeAll();
+    } else {
+        open(name);
     }
 }
