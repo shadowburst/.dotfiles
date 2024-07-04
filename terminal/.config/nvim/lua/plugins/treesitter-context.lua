8d4395886be22884d73f75d3bf75bbd74e1bb369
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-07-04 20:48:00 +0200

    feat(nvim): use ts-comments

---
+++ terminal/.config/nvim/lua/plugins/treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {
+			max_lines = 5,
+			mode = "topline",
+		},
+	},
+}
--- terminal/.config/nvim/lua/plugins/treesitter.lua
+++ terminal/.config/nvim/lua/plugins/treesitter.lua
@@ -2,15 +2,9 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
 			"nvim-treesitter/nvim-treesitter-textobjects",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-			{
-				"nvim-treesitter/nvim-treesitter-context",
-				opts = {
-					max_lines = 5,
-					mode = "topline",
-				},
-			},
+			"folke/ts-comments.nvim",
 		},
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
