813f7ef30187925cf7b8396e02bb1bd69bc86d96
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-07-08 10:02:50 +0200

    fix(ags): hide backdrop blur when needed

--- desktop/.config/ags/modules/windows/backdrop.js
+++ desktop/.config/ags/modules/windows/backdrop.js
@@ -3,7 +3,7 @@ const hyprland = await Service.import('hyprland');
 
 export default function Backdrop(/** @type {boolean} */ dark = false) {
     return Widget.Window({
-        name: 'backdrop',
+        name: dark ? 'backdrop-dark' : 'backdrop',
         classNames: ['backdrop', dark ? 'dark' : ''],
         monitor: hyprland.active.monitor.bind('id'),
         anchor: ['top', 'bottom', 'left', 'right'],
