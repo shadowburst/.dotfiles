4321744ce6e57185bdc7dfa3ed3359f2fbcac1e9
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-07-08 11:18:58 +0200

    docs(ags): jsdoc for array function

--- desktop/.config/ags/utils/array.js
+++ desktop/.config/ags/utils/array.js
@@ -1,3 +1,3 @@
-export function chunk(array, size) {
+export function chunk(/** @type {any[]} */ array, /** @type {number} */ size) {
     return Array.from(Array(Math.ceil(array.length / size)), (_, i) => array.slice(i * size, i * size + size));
 }
