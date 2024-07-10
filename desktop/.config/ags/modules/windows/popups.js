8c340c0ac8819ba865684f8c888f27acf60f7729
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-07-10 07:16:53 +0200

    fix(ags): first notification was cropped

--- desktop/.config/ags/modules/windows/popups.js
+++ desktop/.config/ags/modules/windows/popups.js
@@ -13,29 +13,43 @@ export default function () {
         layer: 'overlay',
         anchor: ['top', 'right'],
         child: Widget.Box({
-            css: 'min-width: 2px; min-height: 2px;',
-            child: Widget.Box({
-                vertical: true,
-                children: notifications.popups.map(Notification),
-            })
-                .hook(
-                    notifications,
-                    (self, /** @type {number} id */ id) => {
-                        const n = notifications.getNotification(id);
+            css: 'min-width: 1px; min-height: 100rem;',
+            vertical: true,
+            children: [
+                Widget.Box({
+                    vertical: true,
+                    children: notifications.popups.map(Notification),
+                })
+                    .hook(
+                        notifications,
+                        (self, /** @type {number} id */ id) => {
+                            if (notifications.dnd) {
+                                return;
+                            }
 
-                        if (n) {
-                            self.children = [Notification(n), ...self.children];
-                        }
-                    },
-                    'notified'
-                )
-                .hook(
-                    notifications,
-                    (self, /** @type {number} id */ id) => {
-                        self.children.find((n) => n.attribute.id === id)?.destroy();
-                    },
-                    'dismissed'
-                ),
+                            const n = notifications.getNotification(id);
+
+                            if (n) {
+                                self.children = [Notification(n), ...self.children];
+                            }
+                        },
+                        'notified'
+                    )
+                    .hook(
+                        notifications,
+                        (self, /** @type {number} id */ id) => {
+                            self.children.find((n) => n.attribute.id === id)?.destroy();
+                        },
+                        'dismissed'
+                    )
+                    .hook(
+                        notifications,
+                        (self, /** @type {number} id */ id) => {
+                            self.children.find((n) => n.attribute.id === id)?.destroy();
+                        },
+                        'closed'
+                    ),
+            ],
         }),
     });
 }
