82d3e58fcfc57d9ba9394aa9c58a152aa1ffccca
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-08-10 00:00:23 +0200

    feat(nvim): change grapple mapping

--- terminal/.config/nvim/lua/plugins/grapple.lua
+++ terminal/.config/nvim/lua/plugins/grapple.lua
@@ -10,7 +10,7 @@ return {
 			},
 		},
 		keys = {
-			{ "<leader>M", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+			{ "<C-m>", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
 			{ "<leader>m", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple tags window" },
 			{ "<S-h>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
 			{ "<S-l>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
