0788dbe5606cebabd9425e88e66c7866f26b89d1
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-08-18 20:45:53 +0200

    feat(nvim): add word with case text object

--- terminal/.config/nvim/lua/plugins/mini-ai.lua
+++ terminal/.config/nvim/lua/plugins/mini-ai.lua
@@ -10,6 +10,15 @@ return {
 					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
 					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
 					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+					e = { -- Word with case
+						{
+							"%u[%l%d]+%f[^%l%d]",
+							"%f[%S][%l%d]+%f[^%l%d]",
+							"%f[%P][%l%d]+%f[^%l%d]",
+							"^[%l%d]+%f[^%l%d]",
+						},
+						"^().*()$",
+					},
 				},
 			}
 		end,
