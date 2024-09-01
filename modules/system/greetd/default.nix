13c5fb96680464919932a10fb2b9cc96516d6a0f
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-09-01 21:40:14 +0200

    feat(greetd): add regreet theme

--- modules/system/greetd/default.nix
+++ modules/system/greetd/default.nix
@@ -1,16 +1,26 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  programs.regreet.enable = true;
-  # security.pam.services.greetd.enableGnomeKeyring = true;
-
-  # systemd.services.greetd.serviceConfig = {
-  #   Type = "idle";
-  #   StandardInput = "tty";
-  #   StandardOutput = "tty";
-  #   StandardError = "journal";
-  #   TTYReset = true;
-  #   TTYVHangup = true;
-  #   TTYVTDisallocate = true;
-  # };
+  programs.regreet = { 
+    enable = true;
+    cursorTheme.name = config.stylix.cursor.name;
+    font.name = config.stylix.fonts.sansSerif.name;
+    iconTheme = {
+      name = "kora";
+      package = pkgs.kora-icon-theme;
+    };
+    theme = {
+      name = "catppuccin-macchiato-blue-standard";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "blue" ];
+        variant = "macchiato";
+      };
+    };
+    settings = {
+      background = {
+        path = ../theme/wallpapers/current.jpg;
+        fit = "Cover";
+      };
+    };
+  };
 }
