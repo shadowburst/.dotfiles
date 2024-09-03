2cec1bbaa2411412613865cf92be2582be7f4fe4
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-09-03 13:12:03 +0200

    feat(greetd): replace regreet with tuigreet

--- modules/system/greetd/default.nix
+++ modules/system/greetd/default.nix
@@ -1,25 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  programs.regreet = { 
+  services.greetd = {
     enable = true;
-    cursorTheme.name = config.stylix.cursor.name;
-    font.name = config.stylix.fonts.sansSerif.name;
-    iconTheme = {
-      name = "kora";
-      package = pkgs.kora-icon-theme;
-    };
-    theme = {
-      name = "catppuccin-macchiato-blue-standard";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "blue" ];
-        variant = "macchiato";
-      };
-    };
     settings = {
-      background = {
-        path = ../theme/wallpapers/current.jpg;
-        fit = "Cover";
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%A %e, %B %Y' --remember --asterisks --cmd Hyprland";
       };
     };
   };
