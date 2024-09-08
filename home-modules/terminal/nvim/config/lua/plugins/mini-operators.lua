87345fb383270efe3144b07051efec09e41e3077
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-09-08 22:27:50 +0200

    feat(nvim): add mini.operators

---
+++ home-modules/terminal/nvim/config/lua/plugins/mini-operators.lua
@@ -0,0 +1,43 @@
+local mappings = {
+	evaluate = "g=",
+	multiply = "m",
+	replace = "x",
+	sort = "gs",
+}
+
+return {
+	{
+		"echasnovski/mini.operators",
+		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+		opts = {
+			evaluate = {
+				prefix = mappings.evaluate,
+			},
+			multiply = {
+				prefix = mappings.multiply,
+			},
+			replace = {
+				prefix = mappings.replace,
+			},
+			sort = {
+				prefix = mappings.sort,
+			},
+		},
+		keys = {
+			{
+				"<S-" .. mappings.multiply .. ">",
+				function()
+					local keys = MiniOperators.multiply() .. vim.api.nvim_replace_termcodes("$", true, true, true)
+					vim.api.nvim_feedkeys(keys, "n", false)
+				end,
+			},
+			{
+				"<S-" .. mappings.replace .. ">",
+				function()
+					local keys = MiniOperators.replace() .. vim.api.nvim_replace_termcodes("$", true, true, true)
+					vim.api.nvim_feedkeys(keys, "n", false)
+				end,
+			},
+		},
+	},
+}
