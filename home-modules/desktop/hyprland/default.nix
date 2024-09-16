9ac34db8607435a660953d33a720ab3414adf0ac
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-09-16 21:46:19 +0200

    fix(hypr): disable vrr for blackscreen

--- home-modules/desktop/hyprland/default.nix
+++ home-modules/desktop/hyprland/default.nix
@@ -106,7 +106,7 @@ in
       };
       misc = {
         disable_hyprland_logo = true;
-        vrr = 2;
+        vrr = 0;
         allow_session_lock_restore = true;
         mouse_move_enables_dpms = true;
         key_press_enables_dpms = true;
