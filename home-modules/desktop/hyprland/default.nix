44cf64023bb9854e407a0c57040bbd485ad805fe
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-10-08 22:17:13 +0200

    fix(hypr): load kanshi on reload

--- home-modules/desktop/hyprland/default.nix
+++ home-modules/desktop/hyprland/default.nix
@@ -40,6 +40,7 @@ in
       ];
       exec = [
         "pkill ags; hyprpanel"
+        "${pkgs.systemd}/bin/systemctl --user reload-or-restart kanshi.service"
       ];
       general = {
         border_size = 2;
