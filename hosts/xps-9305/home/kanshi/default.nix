951b5d3e678aace84092cb9a5661834de81fe9b8
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-10-14 08:44:29 +0200

    feat(kanshi): add work setup

--- hosts/xps-9305/home/kanshi/default.nix
+++ hosts/xps-9305/home/kanshi/default.nix
@@ -37,6 +37,25 @@
           ];
         };
       }
+      {
+        profile = {
+          name = "work";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              mode = "3840x2160@59.997Hz";
+              scale = 2.0;
+              position = "0,120";
+            }
+            {
+              criteria = "DP-2";
+              mode = "3840x1080@59.968Hz";
+              position = "1920,0";
+              scale = 1.0;
+            }
+          ];
+        };
+      }
     ];
   };
 }
