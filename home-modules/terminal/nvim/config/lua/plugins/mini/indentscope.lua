a23b4e615a8bae205c031fe1e88d1b2ef4de68cb
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2024-12-05 20:49:49 +0100

    fix(nvim): hide indentscope on dashboard

--- home-modules/terminal/nvim/config/lua/plugins/mini/indentscope.lua
+++ home-modules/terminal/nvim/config/lua/plugins/mini/indentscope.lua
@@ -21,11 +21,17 @@ return {
 					"lazy",
 					"mason",
 					"notify",
-					"snacks_dashboard",
 					"yazi",
 				},
 				callback = function()
 					vim.b.miniindentscope_disable = true
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "SnacksDashboardOpened",
+				callback = function(data)
+					vim.b[data.buf].miniindentscope_disable = true
 				end,
 			})
 		end,
