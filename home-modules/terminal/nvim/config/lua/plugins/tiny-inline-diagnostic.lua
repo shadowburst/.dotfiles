5e7ff9d3d63ade6b07465cb54855a9c40cfef45e
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-01-01 23:03:42 +0100

    feat(nvim): better diagnostic messages

--- home-modules/terminal/nvim/config/lua/plugins/tiny-inline-diagnostic.lua
+++ home-modules/terminal/nvim/config/lua/plugins/tiny-inline-diagnostic.lua
@@ -4,7 +4,14 @@ return {
 		event = { "VeryLazy" },
 		priority = 1000,
 		opts = {
-			preset = "nonerdfont",
+			options = {
+				multilines = true,
+				show_all_diags_on_cursorline = true,
+			},
+			signs = {
+				left = "",
+				right = "",
+			},
 		},
 		init = function()
 			vim.diagnostic.config({ virtual_text = false })
