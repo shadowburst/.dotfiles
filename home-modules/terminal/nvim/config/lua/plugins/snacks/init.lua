6496fc04f4dca14a22ba5bd3f5ea0814fcf6a9b6
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-01-16 22:17:12 +0100

    fix(nvim): only have one init for snacks

--- home-modules/terminal/nvim/config/lua/plugins/snacks/init.lua
+++ home-modules/terminal/nvim/config/lua/plugins/snacks/init.lua
@@ -7,23 +7,33 @@ return {
 		---@type snacks.Config
 		opts = {
 			bigfile = {},
-			bufdelete = {},
-			gitbrowse = {},
+			debug = {},
 			input = {},
 			quickfile = {},
 			statuscolumn = {},
-			words = {},
 		},
 		init = function()
 			vim.api.nvim_create_autocmd("User", {
 				pattern = "VeryLazy",
 				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
+
 					Snacks.util.on_key("<esc>", function()
 						vim.cmd("noh")
 						if vim.snippet then
 							vim.snippet.stop()
 						end
 					end)
+
+					Snacks.toggle.option("spell", { name = "spelling" }):map("<leader>ts")
+					Snacks.toggle.option("relativenumber", { name = "relative number" }):map("<leader>tl")
+					Snacks.toggle.option("wrap", { name = "wrap" }):map("<leader>tw")
+					Snacks.toggle.diagnostics({ name = "diagnostics" }):map("<leader>td")
+					Snacks.toggle.zen():map("<leader>z")
 				end,
 			})
 		end,
