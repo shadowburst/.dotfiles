127ae4653e567994e0b9b5c86f5e34a3bea89b19
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-01-26 22:10:26 +0100

    feat(nvim): mostly replace fzf-lua with snacks picker

---
+++ home-modules/terminal/nvim/config/lua/plugins/snacks/picker.lua
@@ -0,0 +1,217 @@
+---@type snacks.picker.Config
+local source_config = {
+	hidden = true,
+	ignored = true,
+	exclude = {
+		"storage/**",
+		"node_modules/**",
+		"vendor/**",
+	},
+	filter = { cwd = true },
+}
+
+return {
+	{
+		"folke/snacks.nvim",
+		---@module 'snacks'
+		---@type snacks.Config
+		opts = {
+			picker = {
+				sources = {
+					buffers = source_config,
+					files = source_config,
+					grep = source_config,
+				},
+				layout = { preset = "default" },
+				layouts = {
+					default = {
+						layout = {
+							fullscreen = true,
+							border = "rounded",
+							box = "vertical",
+							{ win = "preview" },
+							{
+								win = "input",
+								height = 1,
+								title = "{source} {live}",
+								title_pos = "center",
+								border = "top",
+							},
+							{
+								win = "list",
+								border = "top",
+								height = 0.4,
+							},
+						},
+					},
+				},
+			},
+			previewers = {
+				git = { native = true },
+			},
+		},
+		keys = {
+			{
+				"<leader><leader>",
+				function()
+					Snacks.picker.smart()
+				end,
+				desc = "Smart find",
+			},
+			{
+				"<leader>,",
+				function()
+					Snacks.picker.buffers()
+				end,
+				desc = "Buffers",
+			},
+			{
+				"<leader>/",
+				function()
+					Snacks.picker.grep()
+				end,
+				desc = "Grep",
+			},
+			{
+				"<leader>:",
+				function()
+					Snacks.picker.command_history()
+				end,
+				desc = "Command history",
+			},
+			{
+				"<leader>.",
+				function()
+					Snacks.picker.resume()
+				end,
+				desc = "Resume",
+			},
+			-- find
+			{
+				"<leader>ff",
+				function()
+					Snacks.picker.files()
+				end,
+				desc = "Find files",
+			},
+			{
+				"<leader>fr",
+				function()
+					Snacks.picker.recent()
+				end,
+				desc = "Recent",
+			},
+			-- git
+			{
+				"<leader>gc",
+				function()
+					Snacks.picker.git_log({ current_file = true })
+				end,
+				desc = "Hunks",
+			},
+			{
+				"<leader>gh",
+				function()
+					Snacks.picker.git_diff()
+				end,
+				desc = "Hunks",
+			},
+			{
+				"<leader>gs",
+				function()
+					Snacks.picker.git_status()
+				end,
+				desc = "Git status",
+			},
+			-- Search
+			{
+				"<leader>sb",
+				function()
+					Snacks.picker.lines()
+				end,
+				desc = "Buffer lines",
+			},
+			{
+				"<leader>sc",
+				function()
+					Snacks.picker.commands()
+				end,
+				desc = "Commands",
+			},
+			{
+				"<leader>sd",
+				function()
+					Snacks.picker.diagnostics()
+				end,
+				desc = "Diagnostics",
+			},
+			{
+				"<leader>sh",
+				function()
+					Snacks.picker.help()
+				end,
+				desc = "Help pages",
+			},
+			{
+				"<leader>sH",
+				function()
+					Snacks.picker.highlights()
+				end,
+				desc = "Highlights",
+			},
+			{
+				"<leader>sk",
+				function()
+					Snacks.picker.keymaps()
+				end,
+				desc = "Keymaps",
+			},
+			{
+				"<leader>sm",
+				function()
+					Snacks.picker.man()
+				end,
+				desc = "Man Pages",
+			},
+			{
+				"<leader>sw",
+				function()
+					Snacks.picker.grep_word({ dirs = { vim.fn.expand("%") } })
+				end,
+				desc = "Visual selection or word",
+				mode = { "n", "x" },
+			},
+			{
+				"<leader>sW",
+				function()
+					Snacks.picker.grep_word()
+				end,
+				desc = "Visual selection or word in cwd",
+				mode = { "n", "x" },
+			},
+			-- LSP
+			{
+				"gd",
+				function()
+					Snacks.picker.lsp_definitions()
+				end,
+				desc = "Goto Definition",
+			},
+			{
+				"gr",
+				function()
+					Snacks.picker.lsp_references()
+				end,
+				nowait = true,
+				desc = "References",
+			},
+			{
+				"<leader>ss",
+				function()
+					Snacks.picker.lsp_symbols()
+				end,
+				desc = "LSP Symbols",
+			},
+		},
+	},
+}
