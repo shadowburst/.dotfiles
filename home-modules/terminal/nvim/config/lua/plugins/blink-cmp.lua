e8bfac5b8b21d13d86d620868f0768f817789437
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-02-16 08:44:40 +0100

    feat(nvim.blink): preselect completion is better

--- home-modules/terminal/nvim/config/lua/plugins/blink-cmp.lua
+++ home-modules/terminal/nvim/config/lua/plugins/blink-cmp.lua
@@ -32,10 +32,7 @@ return {
       },
       completion = {
         list = {
-          selection = {
-            preselect = false,
-            auto_insert = false,
-          },
+          selection = { auto_insert = false },
         },
         menu = {
           draw = {
