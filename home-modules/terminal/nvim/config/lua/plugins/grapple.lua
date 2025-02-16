6afac33be58a425860034926c4639dbbcab5d69c
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-02-16 09:58:33 +0100

    fix(nvim): change grapple keymap, it conflicted with <cr>

--- home-modules/terminal/nvim/config/lua/plugins/grapple.lua
+++ home-modules/terminal/nvim/config/lua/plugins/grapple.lua
@@ -10,7 +10,7 @@ return {
       win_opts = { border = "rounded" },
     },
     keys = {
-      { "<C-m>", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
+      { "<leader>;", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
       { "<leader>m", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple tags window" },
       { "<S-h>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
       { "<S-l>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
