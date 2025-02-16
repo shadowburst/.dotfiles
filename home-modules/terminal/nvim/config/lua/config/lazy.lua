38e8481e68212cbea965b8c0ba00ab9b9d564656
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-02-16 08:48:27 +0100

    refactor(nvim): simplify loading keymaps and autocmds

--- home-modules/terminal/nvim/config/lua/config/lazy.lua
+++ home-modules/terminal/nvim/config/lua/config/lazy.lua
@@ -38,11 +38,3 @@ require("lazy").setup({
     },
   },
 })
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require("config.autocmds")
-    require("config.keymaps")
-  end,
-})
