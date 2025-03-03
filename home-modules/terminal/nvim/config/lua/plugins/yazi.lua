fe1fcba89ba5422d659addcac03c2f108ba53218
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-03 19:11:56 +0100

    feat(nvim.yazi): use for directories

--- home-modules/terminal/nvim/config/lua/plugins/yazi.lua
+++ home-modules/terminal/nvim/config/lua/plugins/yazi.lua
@@ -2,13 +2,16 @@ return {
   {
     "mikavilpas/yazi.nvim",
     dependencies = { "folke/snacks.nvim" },
+    lazy = false,
     ---@module 'yazi'
     ---@type YaziConfig
     opts = {
+      open_for_directories = true,
       highlight_hovered_buffers_in_same_directory = false,
       floating_window_scaling_factor = 1,
       yazi_floating_window_border = "none",
     },
+    init = function() vim.g.loaded_netrwPlugin = 1 end,
     keys = {
       { "<leader>e", "<cmd>Yazi<cr>", desc = "File manager" },
     },
