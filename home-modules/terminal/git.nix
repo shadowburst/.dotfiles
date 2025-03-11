de34adea5d5df4ccf3a07268872d44e6cc98099f
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-11 21:20:58 +0100

    feat(git): add gh cli

--- home-modules/terminal/git.nix
+++ home-modules/terminal/git.nix
@@ -1,21 +1,31 @@
 {...}: {
-  programs.git = {
-    enable = true;
-    userName = "ShadowBurst";
-    userEmail = "37303345+shadowburst@users.noreply.github.com";
-    delta = {
+  programs = {
+    git = {
       enable = true;
-      options = {
-        "line-numbers" = true;
-        "side-by-side" = true;
+      userName = "ShadowBurst";
+      userEmail = "37303345+shadowburst@users.noreply.github.com";
+      delta = {
+        enable = true;
+        options = {
+          "line-numbers" = true;
+          "side-by-side" = true;
+        };
+      };
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+        pull = {
+          rebase = "true";
+        };
       };
     };
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      pull = {
-        rebase = "true";
+
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+        prompt = "enabled";
       };
     };
   };
