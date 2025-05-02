df770cf4ed044a9d733585adb36c5fd56fbf71c0
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-05-02 21:12:19 +0200

    style(kitty): organize options

--- home-modules/desktop/kitty.nix
+++ home-modules/desktop/kitty.nix
@@ -4,17 +4,14 @@
     font.name = config.stylix.fonts.monospace.name;
     shellIntegration.enableFishIntegration = true;
     settings = {
-      cursor_text_color = "background";
-      cursor_shape = "block";
-
-      url_style = "curly";
-      detect_urls = true;
-
-      window_padding_width = "4 0";
-      confirm_os_window_close = 0;
-
       background_opacity = "1";
+      confirm_os_window_close = 0;
+      cursor_shape = "block";
+      cursor_text_color = "background";
       cursor_trail = 3;
+      detect_urls = true;
+      url_style = "curly";
+      window_padding_width = "4 0";
     };
     themeFile = "Catppuccin-Macchiato";
   };
