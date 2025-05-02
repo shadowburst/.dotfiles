60a5c0d1175c14f2b38eeee8dfac3212f75a4d00
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-05-02 22:22:29 +0200

    fix(cosmic): enable gnupg on login

--- nixos-modules/laptop/cosmic.nix
+++ nixos-modules/laptop/cosmic.nix
@@ -1,5 +1,8 @@
 {...}: {
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
-  security.pam.services.cosmic-greeter.enableGnomeKeyring = true;
+  security.pam.services.cosmic-greeter = {
+    enableGnomeKeyring = true;
+    gnupg.enable = true;
+  };
 }
