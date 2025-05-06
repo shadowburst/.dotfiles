8f97459388a47ca6522b166912f7d46f46b93401
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-05-07 00:07:04 +0200

    feat(steam): wrap binary to offload gpu

--- nixos-modules/laptop/gaming.nix
+++ nixos-modules/laptop/gaming.nix
@@ -1,10 +1,29 @@
-{...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with pkgs; let
+  patchDesktop = pkg: appName: from: to:
+    lib.hiPrio (
+      pkgs.runCommand "$patched-desktop-entry-for-${appName}" {} ''
+        ${coreutils}/bin/mkdir -p $out/share/applications
+        ${gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+      ''
+    );
+  GPUOffloadApp = pkg: desktopName:
+    lib.mkIf config.hardware.nvidia.prime.offload.enable
+    (patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ");
+in {
   programs = {
     gamemode.enable = true;
     steam = {
       enable = true;
-      gamescopeSession.enable = true;
       remotePlay.openFirewall = true;
     };
   };
+  environment.systemPackages = with pkgs; [
+    (GPUOffloadApp steam "steam")
+  ];
 }
