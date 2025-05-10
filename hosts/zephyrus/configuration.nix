66f6250a5e9c370677a58c15cdb329cb76652943
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-05-10 10:54:25 +0200

    feat(zephyrus): better nvidia management

--- hosts/zephyrus/configuration.nix
+++ hosts/zephyrus/configuration.nix
@@ -36,19 +36,48 @@
       enable = true;
       enable32Bit = true;
     };
+    nvidia = {
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+      dynamicBoost.enable = true;
+      open = true;
+    };
   };
 
   networking.hostName = "zephyrus";
+
+  systemd.extraConfig = "DefaultTimeoutStopSec=10s";
 
   services.asusd = {
     asusdConfig.source = ./config/asusd.ron;
     auraConfigs."19b6".source = ./config/aura_19b6.ron;
   };
-
   environment.etc."asusd/slash.ron" = {
     source = ./config/slash.ron;
     mode = "0644";
   };
 
-  systemd.extraConfig = "DefaultTimeoutStopSec=10s";
+  environment.variables = {
+    GSK_RENDERER = "ngl";
+    LIBVA_DRIVER_NAME = "radeonsi";
+    VDPAU_DRIVER = "radeonsi";
+    VK_DRIVER_FILES = "/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json:/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+    __EGL_VENDOR_LIBRARY_FILENAMES = "/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json";
+    __GLX_VENDOR_LIBRARY_NAME = "mesa";
+    __NV_PRIME_RENDER_OFFLOAD = 0;
+    __VK_LAYER_NV_optimus = "non_NVIDIA_only";
+  };
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "nvidia-offload" ''
+      export VK_DRIVER_FILES=/run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json:/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json
+      export __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/10_nvidia.json
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec "$@"
+    '')
+  ];
 }
