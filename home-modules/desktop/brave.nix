b5f162a727a049559fde8feda6478bf7b0196d68
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-06-17 21:06:14 +0200

    refactor(brave): cleaner install

--- home-modules/desktop/brave.nix
+++ home-modules/desktop/brave.nix
@@ -3,10 +3,15 @@
     BROWSER = "brave";
   };
 
-  home.packages = with pkgs; [
-    (brave.override {
+  programs.brave = {
+    enable = true;
+    package = pkgs.brave.override {
       vulkanSupport = true;
-      commandLineArgs = "--enable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,TouchpadOverscrollHistoryNavigation --ozone-platform-hint=auto --password-store=gnome-libsecret";
-    })
-  ];
+    };
+    commandLineArgs = [
+      "--enable-features=AcceleratedVideoDecodeLinuxGL,AcceleratedVideoEncoder,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,TouchpadOverscrollHistoryNavigation"
+      "--ozone-platform-hint=auto"
+      "--password-store=gnome-libsecret"
+    ];
+  };
 }
