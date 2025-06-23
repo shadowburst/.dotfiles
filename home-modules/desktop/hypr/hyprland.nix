4cbf713a56ce284da8bcebc8bb447d58bc9ebe26
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-06-23 23:37:53 +0200

    feat(hypr): add group bar

--- home-modules/desktop/hypr/hyprland.nix
+++ home-modules/desktop/hypr/hyprland.nix
@@ -80,6 +80,18 @@
       dwindle = {
         force_split = 2;
       };
+      group = {
+        groupbar = {
+          font_size = 15;
+          gradients = true;
+          gradient_round_only_edges = false;
+          gradient_rounding = 5;
+          height = 25;
+          indicator_height = 0;
+          gaps_in = 3;
+          gaps_out = 3;
+        };
+      };
       xwayland = {
         force_zero_scaling = true;
       };
