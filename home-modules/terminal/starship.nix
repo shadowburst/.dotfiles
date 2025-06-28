f7c2376295830e2bcac6f49ec6efed023bea52c0
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-06-28 21:36:09 +0200

    feat(starship): add time

--- home-modules/terminal/starship.nix
+++ home-modules/terminal/starship.nix
@@ -3,5 +3,13 @@
     enable = true;
     enableFishIntegration = true;
     enableBashIntegration = true;
+    settings = {
+      right_format = "$time";
+      time = {
+        disabled = false;
+        time_format = "%R";
+        format = "[   $time ]($style)";
+      };
+    };
   };
 }
