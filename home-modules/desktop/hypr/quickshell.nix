4ed72a2ba0fe8d34d8da06530c65bd19f19d024a
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-06-29 22:01:27 +0200

    build(nixos): update flake

--- home-modules/desktop/hypr/quickshell.nix
+++ home-modules/desktop/hypr/quickshell.nix
@@ -2,8 +2,8 @@
   xdg.configFile."quickshell".source = pkgs.fetchFromGitHub {
     owner = "caelestia-dots";
     repo = "shell";
-    rev = "ce3467c8e22e2ea1b266e6ad5d12eefc5541aad4";
-    sha256 = "15rxh85gyfgciwkkmp066gw2yd9n1d3n3mskmggmwi67qxdb0w36";
+    rev = "0d7804f67723107f4e78beca8229d2fbb82ce4a3";
+    sha256 = "1r1hny5jbaqiqb4n7210v9s6rd4wz52c99y6n26rbid56d2ndj68";
   };
   xdg.configFile."caelestia/shell.json".text = builtins.toJSON {
     bar = {
@@ -30,6 +30,7 @@
     app2unit
     libqalculate
     lm_sensors
+    inotify-tools
     material-symbols
   ];
 }
