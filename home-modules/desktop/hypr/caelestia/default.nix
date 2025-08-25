6066325e63e61a298a06c03df50663b50e05b933
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-08-25 20:21:22 +0200

    feat(caelestia): visualiser and better launcher action prefix

--- home-modules/desktop/hypr/caelestia/default.nix
+++ home-modules/desktop/hypr/caelestia/default.nix
@@ -1,74 +1,60 @@
-{ config, ... }:
-{
+{config, ...}: {
   programs.caelestia = {
     enable = true;
     cli.enable = true;
     settings = {
+      appearance.font.sans = config.stylix.fonts.sansSerif.name;
+      appearance.font.mono = config.stylix.fonts.monospace.name;
+      background.visualiser.autoHide = false;
+      bar.status.showAudio = true;
+      bar.workspaces.shown = 7;
+      bar.workspaces.occupiedBg = true;
+      bar.workspaces.activeTrail = true;
+      bar.workspaces.occupiedLabel = " ";
+      bar.workspaces.activeLabel = " ";
+      bar.entries = [
+        {
+          id = "logo";
+          enabled = true;
+        }
+        {
+          id = "workspaces";
+          enabled = true;
+        }
+        {
+          id = "spacer";
+          enabled = true;
+        }
+        {
+          id = "activeWindow";
+          enabled = true;
+        }
+        {
+          id = "spacer";
+          enabled = true;
+        }
+        {
+          id = "tray";
+          enabled = true;
+        }
+        {
+          id = "idleInhibitor";
+          enabled = true;
+        }
+        {
+          id = "statusIcons";
+          enabled = true;
+        }
+        {
+          id = "clock";
+          enabled = true;
+        }
+      ];
+      border.thickness = 1;
+      border.rounding = 12;
       general.apps.terminal = config.home.sessionVariables.TERMINAL;
-      appearance = {
-        font = {
-          sans = config.stylix.fonts.sansSerif.name;
-          mono = config.stylix.fonts.monospace.name;
-        };
-      };
-      bar = {
-        status.showAudio = true;
-        workspaces = {
-          shown = 7;
-          occupiedBg = true;
-          activeTrail = true;
-          occupiedLabel = " ";
-          activeLabel = " ";
-        };
-        entries = [
-          {
-            id = "logo";
-            enabled = true;
-          }
-          {
-            id = "workspaces";
-            enabled = true;
-          }
-          {
-            id = "spacer";
-            enabled = true;
-          }
-          {
-            id = "activeWindow";
-            enabled = true;
-          }
-          {
-            id = "spacer";
-            enabled = true;
-          }
-          {
-            id = "tray";
-            enabled = true;
-          }
-          {
-            id = "clock";
-            enabled = true;
-          }
-          {
-            id = "idleInhibitor";
-            enabled = true;
-          }
-          {
-            id = "statusIcons";
-            enabled = true;
-          }
-          {
-            id = "power";
-            enabled = true;
-          }
-
-        ];
-      };
-      border = {
-        thickness = 1;
-        rounding = 12;
-      };
       launcher.vimKeybinds = true;
+      launcher.actionPrefix = " ";
       services.smartScheme = false;
       services.weatherLocation = "48.306453773398786, -0.6214670156648004";
       session.vimKeybinds = true;
