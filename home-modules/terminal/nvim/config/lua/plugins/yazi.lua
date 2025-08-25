ad3f9a4a85da78afba9597c6afd3667032be2f59
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-08-25 21:34:29 +0200

    fix(nvim): better yazi keymaps

--- home-modules/terminal/nvim/config/lua/plugins/yazi.lua
+++ home-modules/terminal/nvim/config/lua/plugins/yazi.lua
@@ -10,6 +10,14 @@ return {
       highlight_hovered_buffers_in_same_directory = false,
       floating_window_scaling_factor = 1,
       yazi_floating_window_border = "none",
+      integrations = {
+        grep_in_directory = "snacks.picker",
+        grep_in_selected_files = "snacks.picker",
+      },
+      keymaps = {
+        grep_in_directory = "<c-g>",
+        replace_in_directory = "<c-r>",
+      },
     },
     init = function() vim.g.loaded_netrwPlugin = 1 end,
     keys = {
