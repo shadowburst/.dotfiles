25c80785acb97594e947389b5d718f8371bf1220
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-01 20:33:54 +0200

    feat(nvim): open opencode in tmux if possible

--- home-modules/terminal/nvim/config/lua/plugins/opencode.lua
+++ home-modules/terminal/nvim/config/lua/plugins/opencode.lua
@@ -3,13 +3,19 @@ return {
     "NickvanDyke/opencode.nvim",
     dependencies = {
       "folke/snacks.nvim",
+      "aserowy/tmux.nvim",
     },
     ---@type opencode.Opts
     opts = {
       auto_reload = true,
       on_opencode_not_found = function()
-        vim.notify("Opencode instance not found", vim.log.levels.WARN, { title = "opencode" })
-        return false
+        local tmux = require("tmux.wrapper.tmux")
+        local success = tmux.execute("split-window -h -l 80 opencode")
+        if not success then
+          vim.notify("Failed to open tmux pane", vim.log.levels.ERROR, { title = "opencode" })
+          return false
+        end
+        return true
       end,
     },
     keys = {
