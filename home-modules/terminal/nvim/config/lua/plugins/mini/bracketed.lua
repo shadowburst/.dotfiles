a125b9632032cc88f0953b6579cfe8f61cbef69b
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-15 22:17:53 +0200

    fix(nvim): use snacks for indent navigation

--- home-modules/terminal/nvim/config/lua/plugins/mini/bracketed.lua
+++ home-modules/terminal/nvim/config/lua/plugins/mini/bracketed.lua
@@ -3,6 +3,7 @@ return {
     "nvim-mini/mini.bracketed",
     event = { "BufNewFile", "BufReadPost", "BufWritePre" },
     opts = {
+      indent = { suffix = "" },
       undo = { suffix = "" },
     },
   },
