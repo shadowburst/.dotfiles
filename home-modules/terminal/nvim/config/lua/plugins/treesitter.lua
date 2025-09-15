2b587c7f5c9f5c1a24bc9300eb181b5006f98673
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-15 23:01:20 +0200

    feat(nvim): enable incremental selection

--- home-modules/terminal/nvim/config/lua/plugins/treesitter.lua
+++ home-modules/terminal/nvim/config/lua/plugins/treesitter.lua
@@ -43,6 +43,13 @@ return {
       },
       highlight = { enable = true },
       indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = "v",
+          node_decremental = "V",
+        },
+      },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
