95faceb859c383cb4acb4b7d02d6ccf3c489870a
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-17 23:32:35 +0200

    feat(ghostty): add keybinds for fast delete words

--- home-modules/desktop/ghostty.nix
+++ home-modules/desktop/ghostty.nix
@@ -11,7 +11,9 @@
       confirm-close-surface = false;
       window-padding-y = 2;
       keybind = [
-        "shift+enter=csi:13;3u"
+        "shift+enter=text:\\x1b\\r" # Send Alt+Enter
+        "control+backspace=text:\\x1b\\x7f" # Send Alt+Backspace
+        "control+delete=text:\\x1b[3;3~" # Send Alt+Delete
       ];
     };
   };
