c93ac29922b74504a2581c23fff72f4cddc29d72
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-22 20:06:04 +0200

    refactor(nvim): simplify catppuccin integrations

--- home-modules/terminal/nvim/config/lua/plugins/catppuccin.lua
+++ home-modules/terminal/nvim/config/lua/plugins/catppuccin.lua
@@ -33,41 +33,7 @@ return {
         }
       end,
       default_integrations = false,
-      integrations = {
-        blink_cmp = true,
-        dashboard = true,
-        diffview = true,
-        flash = true,
-        gitsigns = true,
-        grug_far = true,
-        lsp_trouble = true,
-        mini = { enabled = true },
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-            ok = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-            ok = { "underline" },
-          },
-          inlay_hints = { background = true },
-        },
-        neogit = true,
-        noice = true,
-        render_markdown = true,
-        snacks = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
+      auto_integrations = true,
     },
     config = function(_, opts)
       require("catppuccin").setup(opts)
