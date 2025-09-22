c7fbbfa7f8c2073b49b6b0cbfff164a19083d5e0
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-22 08:12:48 +0200

    feat(opencode): remove unused prompts and better theme

--- home-modules/terminal/opencode/default.nix
+++ home-modules/terminal/opencode/default.nix
@@ -4,10 +4,6 @@
     settings = {
       theme = "catppuccin";
       model = "github-copilot/gpt-4.1";
-      mode = {
-        build.prompt = "{file:${./prompts/build.md}}";
-        plan.prompt = "{file:${./prompts/plan.md}}";
-      };
       keybinds = {
         messages_half_page_up = "ctrl+u";
         messages_half_page_down = "ctrl+d";
