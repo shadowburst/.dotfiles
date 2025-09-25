c9b48b520c3798fb773681d0cbe53429e9da0da6
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-09-25 22:06:59 +0200

    feat(ghostty): decrease font and fix ctrl-enter

--- home-modules/desktop/ghostty/default.nix
+++ home-modules/desktop/ghostty/default.nix
@@ -6,7 +6,7 @@
         fonts.monospace.name
         fonts.emoji.name
       ];
-      font-size = 11;
+      font-size = 10;
       cursor-color = config.lib.stylix.colors.withHashtag.base07;
       background-opacity = 0.9;
       confirm-close-surface = false;
@@ -14,8 +14,9 @@
       window-padding-y = 2;
       window-padding-color = "extend";
       keybind = [
+        "ctrl+enter=unbind" # Send Control+Enter
         "shift+enter=text:\\x1b\\r" # Send Alt+Enter
-        "control+backspace=text:\\x1b\\x7f" # Send Alt+Backspace
+        "ctrl+backspace=text:\\x1b\\x7f" # Send Alt+Backspace
       ];
       custom-shader = [
         "${./shaders/cursor_smear.glsl}"
