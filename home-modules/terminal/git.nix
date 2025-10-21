264bae1fb08ab637351710ac0a72b9e9b0a9286e
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-10-21 12:44:15 +0200

    chore(git): update home manager options

--- home-modules/terminal/git.nix
+++ home-modules/terminal/git.nix
@@ -1,33 +1,35 @@
 {pkgs, ...}: {
-  programs = {
-    git = {
-      enable = true;
-      userName = "ShadowBurst";
-      userEmail = "37303345+shadowburst@users.noreply.github.com";
-      delta = {
-        enable = true;
-        options = {
-          "line-numbers" = true;
-          "side-by-side" = true;
-        };
-      };
-      extraConfig = {
-        init.defaultBranch = "main";
-        pull.rebase = "true";
+  programs.git = {
+    enable = true;
+    settings = {
+      init.defaultBranch = "main";
+      pull.rebase = "true";
+      user = {
+        name = "ShadowBurst";
+        email = "37303345+shadowburst@users.noreply.github.com";
       };
     };
-
-    gh = {
-      enable = true;
-      settings = {
-        git_protocol = "ssh";
-        prompt = "enabled";
-      };
-      extensions = with pkgs; [gh-copilot];
-    };
-
-    gh-dash.enable = true;
   };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      "line-numbers" = true;
+      "side-by-side" = true;
+    };
+  };
+
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+      prompt = "enabled";
+    };
+    extensions = with pkgs; [gh-copilot];
+  };
+
+  programs.gh-dash.enable = true;
 
   home.packages = with pkgs; [
     wrkflw
