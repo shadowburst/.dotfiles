1593398cae98a385d7e46cae7ddbe1416abf3fb3
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-10-27 20:39:02 +0100

    feat(nvim): add neogit log refresh keymap

--- home-modules/terminal/nvim/config/lua/plugins/neogit.lua
+++ home-modules/terminal/nvim/config/lua/plugins/neogit.lua
@@ -30,7 +30,6 @@ return {
       },
     },
     init = function()
-      local group = vim.api.nvim_create_augroup("custom_neogit", { clear = true })
       vim.api.nvim_create_autocmd("User", {
         pattern = {
           "NeogitBranchCheckout",
@@ -49,13 +48,29 @@ return {
           "NeogitTagCreate",
           "NeogitTagDelete",
         },
-        group = group,
+        group = vim.api.nvim_create_augroup("neogit.reload_log_auto", { clear = true }),
         callback = function(event)
           local buffername = vim.api.nvim_buf_get_name(event.buf)
           if buffername:match("NeogitLogView$") then
             vim.fn.feedkeys("q", "x")
-            require("neogit").action("log", "log_all_branches", { "--graph", "--decorate" })()
+            vim.schedule(
+              function() require("neogit").action("log", "log_all_branches", { "--graph", "--decorate" })() end
+            )
           end
+        end,
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("neogit.reload_log_manual", { clear = true }),
+        pattern = {
+          "NeogitLogView",
+        },
+        callback = function(event)
+          vim.keymap.set(
+            "n",
+            "<c-r>",
+            function() require("neogit").action("fetch", "fetch_all_remotes", { "--prune" })() end,
+            { buffer = event.buf, silent = true }
+          )
         end,
       })
     end,
