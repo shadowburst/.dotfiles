898e2e890b5c86feeec48df40a1fec37af616613
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-01-21 22:57:31 +0100

    feat(nvim): use sidekick plugin instead of opencode

--- home-modules/terminal/nvim/config/lua/plugins/sidekick.lua
+++ home-modules/terminal/nvim/config/lua/plugins/sidekick.lua
@@ -6,7 +6,20 @@ return {
     ---@module 'sidekick'
     ---@class sidekick.Config
     opts = {
-      cli = { enabled = false },
+      cli = {
+        win = {
+          keys = {
+            files = { "@", "files", mode = "nt", desc = "open file picker" },
+            hide_n = { "<esc>", "hide", mode = "n", desc = "hide the terminal window" },
+            stopinsert = { "<esc>", "stopinsert", mode = "t", desc = "enter normal mode" },
+          },
+        },
+        tools = {
+          opencode = {
+            env = { OPENCODE_THEME = "" }, -- No need to change theme, it works in tmux
+          },
+        },
+      },
       nes = {
         enabled = function(buf)
           return vim.g.sidekick_nes ~= false and vim.b.sidekick_nes ~= false and vim.fn.mode() ~= "s"
@@ -23,6 +36,33 @@ return {
         end,
         expr = true,
         desc = "Next edit suggestion",
+      },
+      {
+        "<leader>oo",
+        function()
+          if #require("sidekick.status").cli() > 0 then
+            require("sidekick.cli").focus()
+          else
+            require("sidekick.cli").toggle({
+              name = "opencode",
+              focus = true,
+            })
+          end
+        end,
+        mode = { "n", "x" },
+        desc = "Toggle opencode",
+      },
+      {
+        "<leader>op",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Prompt opencode",
+      },
+      {
+        "<leader>os",
+        function() require("sidekick.cli").send("{selection}") end,
+        mode = { "x" },
+        desc = "Send selection to opencode",
       },
     },
   },
--- home-modules/terminal/opencode.nix
+++ home-modules/terminal/opencode.nix
@@ -4,6 +4,7 @@
     settings = {
       permission.edit = "ask";
       keybinds = {
+        command_list = "<leader>p";
         messages_half_page_up = "ctrl+u";
         messages_half_page_down = "ctrl+d";
         input_newline = "shift+enter";
