3cf59f49ca06f3d959acf2cf7aced675fe2a1d57
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-02-03 20:34:16 +0100

    fix(nvim): disable <cr> in certain filetypes

--- home-modules/terminal/nvim/config/lua/plugins/flash.lua
+++ home-modules/terminal/nvim/config/lua/plugins/flash.lua
@@ -16,11 +16,29 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("flash").setup(opts)
+
+      local gr = vim.api.nvim_create_augroup("custom.flash", {})
+      local au = function(event, pattern, callback, desc)
+        vim.api.nvim_create_autocmd(event, { pattern = pattern, group = gr, callback = callback, desc = desc })
+      end
+      local revert_cr = function() vim.keymap.set("n", "<CR>", "<CR>", { buffer = true }) end
+      au("FileType", "qf", revert_cr, "Revert <CR>")
+      au("CmdwinEnter", "*", revert_cr, "Revert <CR>")
+    end,
+
     keys = {
       {
         "<cr>",
         mode = { "n" },
         function() require("flash").jump() end,
+        desc = "Flash jump",
+      },
+      {
+        "<s-cr>",
+        mode = { "n" },
+        function() require("flash").jump({ continue = true }) end,
         desc = "Flash jump",
       },
       {
