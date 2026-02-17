0d33b16492f3f551c3c0d5aab575638d1f695397
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-02-17 20:54:26 +0100

    fix(zephyrus): temporary freeze kernel version for nvidia build errors

--- hosts/zephyrus/configuration.nix
+++ hosts/zephyrus/configuration.nix
@@ -12,7 +12,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_6_18;
 
     initrd.luks.devices."luks-110fffaf-55a6-4c06-b218-0896a9217436".device = "/dev/disk/by-uuid/110fffaf-55a6-4c06-b218-0896a9217436";
     loader = {
