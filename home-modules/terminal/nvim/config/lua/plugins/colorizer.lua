21cdcd980bb2164b546fa5b1f9ea06e6bb7e4fa0
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-03-06 07:22:46 +0100

    chore(nvim): update colorizer config

--- home-modules/terminal/nvim/config/lua/plugins/colorizer.lua
+++ home-modules/terminal/nvim/config/lua/plugins/colorizer.lua
@@ -17,17 +17,14 @@ return {
         "typescript.tsx",
         "vue",
       },
-      options = {
-        parsers = {
-          css = true,
-          tailwind = {
-            enable = true,
-            lsp = true,
-            update_names = true,
-          },
+      parsers = {
+        css = true,
+        tailwind = {
+          enable = true,
+          lsp = true,
+          update_names = true,
         },
       },
-      user_default_options = { suppress_deprecation = true },
     },
     config = function(_, opts)
       local c = require("colorizer")
