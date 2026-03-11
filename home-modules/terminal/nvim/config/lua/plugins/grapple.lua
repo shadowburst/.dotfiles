b2c6931ba440352a44752c320c7273de00dcdd3e
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-03-11 19:55:03 +0100

    feat(nvim): simplify grapple config

--- home-modules/terminal/nvim/config/lua/plugins/grapple.lua
+++ home-modules/terminal/nvim/config/lua/plugins/grapple.lua
@@ -14,43 +14,8 @@ return {
     keys = {
       { "<s-h>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
       { "<s-l>", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
-      {
-        "m",
-        function()
-          local char = vim.fn.getcharstr()
-          -- Handle ESC, Ctrl-C, etc.
-          if char == "" or vim.startswith(char, "<") then
-            return
-          end
-          local grapple = require("grapple")
-          grapple.tag({ name = char })
-          local filepath = vim.api.nvim_buf_get_name(0)
-          local filename = vim.fn.fnamemodify(filepath, ":t")
-          vim.notify("Marked " .. filename .. " as " .. char)
-        end,
-        desc = "Grapple save mark",
-        noremap = true,
-        silent = true,
-      },
-      {
-        "'",
-        function()
-          local char = vim.fn.getcharstr()
-          -- Handle ESC, Ctrl-C, etc.
-          if char == "" or vim.startswith(char, "<") then
-            return
-          end
-          local grapple = require("grapple")
-          if char == "'" then
-            grapple.toggle_tags()
-            return
-          end
-          grapple.select({ name = char })
-        end,
-        desc = "Grapple open mark",
-        noremap = true,
-        silent = true,
-      },
+      { "<leader>m", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple tags window" },
+      { "m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
     },
   },
 }
