9cb41524b952d4544a9998e228e4d62464771624
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-03-13 08:10:13 +0100

    feat(nvim): better grug far keymaps

--- home-modules/terminal/nvim/config/lua/plugins/grug-far.lua
+++ home-modules/terminal/nvim/config/lua/plugins/grug-far.lua
@@ -5,67 +5,37 @@ return {
     ---@module 'grug-far'
     ---@type grug.far.OptionsOverride
     opts = {
-      startCursorRow = 2,
       headerMaxWidth = 80,
       transient = true,
+      visualSelectionUsage = "auto-detect",
     },
     keys = {
       {
         "<leader>sr",
         function()
           require("grug-far").open({
+            startCursorRow = vim.fn.mode() == "v" and 2 or 1,
             prefills = {
-              search = vim.fn.expand("<cword>"),
               paths = vim.fn.expand("%"),
             },
           })
         end,
-        desc = "Replace <cword> in file",
-      },
-      {
-        "<leader>sr",
-        function()
-          if vim.fn.mode() ~= "V" then
-            require("grug-far").open({
-              prefills = {
-                paths = vim.fn.expand("%"),
-              },
-            })
-          else
-            require("grug-far").open({
-              startCursorRow = 1,
-              visualSelectionUsage = "operate-within-range",
-            })
-          end
-        end,
-        mode = { "x" },
-        desc = "Replace in selection",
+        mode = { "n", "x" },
+        desc = "Replace in file",
       },
       {
         "<leader>sR",
         function()
           require("grug-far").open({
-            prefills = {
-              search = vim.fn.expand("<cword>"),
-              filesFilter = "!.git/",
-              flags = "--hidden",
-            },
-          })
-        end,
-        desc = "Replace <cword> in files",
-      },
-      {
-        "<leader>sR",
-        function()
-          require("grug-far").open({
+            startCursorRow = vim.fn.mode() == "v" and 2 or 1,
             prefills = {
               filesFilter = "!.git/",
               flags = "--hidden",
             },
           })
         end,
-        mode = { "x" },
-        desc = "Replace selection in files",
+        mode = { "n", "x" },
+        desc = "Replace in files",
       },
     },
   },
