7dfaf12339443529f7c20e4aeb7265e6441f9559
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-04-08 18:05:59 +0200

    fix(nvim): correct grapple keymaps

--- modules/terminal/neovim/config/plugin/50-grapple.lua
+++ modules/terminal/neovim/config/plugin/50-grapple.lua
@@ -7,7 +7,7 @@ require("grapple").setup({
   win_opts = { border = vim.g.border },
 })
 
-vim.keymap.set("n", "<leader>mm", "<cmd>Grapple toggle_tags<cr>", { desc = "Grapple tags window" })
+vim.keymap.set("n", "<leader>m", "<cmd>Grapple toggle_tags<cr>", { desc = "Grapple tags window" })
 vim.keymap.set("n", "m", "<cmd>Grapple toggle<cr>", { desc = "Grapple toggle tag" })
 vim.keymap.set("n", "<s-h>", "<cmd>Grapple cycle_tags prev<cr>", { desc = "Grapple cycle previous tag" })
 vim.keymap.set("n", "<s-l>", "<cmd>Grapple cycle_tags next<cr>", { desc = "Grapple cycle next tag" })
