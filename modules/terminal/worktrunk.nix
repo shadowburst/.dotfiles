f5cbe2f2b17c9f45c2ffcfcb2876eb08289ead30
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-04-12 23:25:21 +0200

    feat(worktrunk): always add new worktree to zoxide

--- modules/terminal/worktrunk.nix
+++ modules/terminal/worktrunk.nix
@@ -10,9 +10,13 @@
         worktrunk
       ];
 
-      # xdg.configFile."worktrunk/config.toml".source = tomlFormat.generate "worktrunk-config.toml" {
-      #   commit.generation.command = "opencode run -m github-copilot/gpt-5-mini";
-      # };
+      xdg.configFile."worktrunk/config.toml".source = tomlFormat.generate "worktrunk-config.toml" {
+        commit.generation.command = "CLAUDECODE= MAX_THINKING_TOKENS=0 claude -p --no-session-persistence --model=haiku --tools='' --disable-slash-commands --setting-sources='' --system-prompt=''";
+        # commit.generation.command = "opencode run -m github-copilot/gpt-5-mini";
+        post-switch = {
+          zoxide = "zoxide add {{ worktree_path }}";
+        };
+      };
 
       programs.fish.interactiveShellInit = ''
         ${lib.getExe pkgs.worktrunk} config shell init fish | source
