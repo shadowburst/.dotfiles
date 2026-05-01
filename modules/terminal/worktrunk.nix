fdaca1914ff4017332e36a35da08d85da079e3ab
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-05-01 23:40:25 +0200

    fix(worktrunk): make it play nice with lerd

--- modules/terminal/worktrunk.nix
+++ modules/terminal/worktrunk.nix
@@ -12,6 +12,7 @@ _: {
       xdg.configFile."worktrunk/config.toml".source = tomlFormat.generate "worktrunk-config.toml" {
         commit.generation.command = "CLAUDECODE= MAX_THINKING_TOKENS=0 claude -p --no-session-persistence --model=haiku --tools='' --disable-slash-commands --setting-sources='' --system-prompt=''";
         # commit.generation.command = "opencode run -m github-copilot/gpt-5-mini";
+        worktree-path = "{{ repo_path }}/../{{ repo }}-{{ branch | sanitize }}";
         post-switch = {
           zoxide = "zoxide add {{ worktree_path }}";
         };
