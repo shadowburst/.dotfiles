cd112f09e78c24ca43cf3d1b5357757e8c5ec076
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-05-25 21:40:33 +0200

    feat(pi): block executing python, prefer bash/gnu tools

--- modules/terminal/pi.nix
+++ modules/terminal/pi.nix
@@ -33,6 +33,7 @@ _: {
       home.packages = [ piWithNpmExtensions ];
 
       home.file = {
+        ".pi/agent/APPEND_SYSTEM.md" = mkPiConfigSymlink "config/pi/APPEND_SYSTEM.md";
         ".pi/agent/themes" = mkPiConfigSymlink "config/pi/themes";
         ".pi/agent/settings.json" = mkPiConfigSymlink "config/pi/settings.json";
         ".pi/agent/keybindings.json" = mkPiConfigSymlink "config/pi/keybindings.json";
