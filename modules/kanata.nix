55f2a8a73824a88ad1fb8ef495c30489bb51889d
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-06-13 20:44:49 +0200

    feat(kanata): add gaming layer with toggle chord

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -17,6 +17,7 @@
           '';
           config = ''
             (defsrc
+              esc
               grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
               tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
               caps a    s    d    f    g    h    j    k    l    ;    '    ret
@@ -31,6 +32,14 @@
 
             (defalias
               spc (tap-hold $tap-time $hold-time spc ralt)
+              toggle-gaming (switch
+                ((layer default)) (layer-switch gaming) break
+                ((layer gaming)) (layer-switch default) break
+              )
+              toggle-esc (switch
+                ((and (input real lctl) (input real lmet))) @toggle-gaming break
+                () esc break
+              )
               caps (fork esc caps (lsft rsft))
 
               a (tap-hold $tap-time $hold-time a lalt)
@@ -44,11 +53,21 @@
              )
 
             (deflayer default
+              @toggle-esc
               grv   1    2    3    4    5    6    7    8    9    0    -    =    bspc
               tab   q    w    e    r    t    y    u    i    o    p    [    ]    \
               @caps @a   @s   @d   @f   g    h    @j   @k   @l   @;   '    ret
               lsft  z    x    c    v    b    n    m    ,    .    /    rsft
               lctl  lmet lalt           @spc           ralt rmet rctl
+            )
+
+            (deflayer gaming
+              @toggle-esc
+              grv   1    2    3    4    5    6    7    8    9    0    -    =    bspc
+              tab   q    w    e    r    t    y    u    i    o    p    [    ]    \
+              @caps a    s    d    f    g    h    j    k    l    ;    '    ret
+              lsft  z    x    c    v    b    n    m    ,    .    /    rsft
+              lctl  lmet lalt           @spc          ralt rmet rctl
             )
           '';
         };
