093f061ab8eaa48b06f91af345fa52c31e84bc21
pbaudry <p.baudry@lynx-business.com>
2026-07-08 10:18:32 +0200

    fix(nvim): allow moving neovim windows

--- config/neovim/plugin/20-keymaps.lua
+++ config/neovim/plugin/20-keymaps.lua
@@ -38,3 +38,8 @@ Snacks.keymap.set(
   function() vim.pack.update(nil, { target = "lockfile" }) end,
   { desc = "Reset plugins" }
 )
+
+Snacks.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+Snacks.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+Snacks.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+Snacks.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
