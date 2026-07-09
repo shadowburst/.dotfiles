de40ce1e0dfeef4079927ac352f1c1b708a642c5
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-07-09 08:17:24 +0200

    chore(nvim): update plugins and remove pi

--- config/neovim/plugin/50-pi.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({
-  "https://github.com/pablopunk/pi.nvim",
-})
-
-require("pi").setup()
-
-vim.keymap.set("n", "<leader>a", ":PiAsk<CR>", { desc = "Ask pi" })
-vim.keymap.set("v", "<leader>a", ":PiAskSelection<CR>", { desc = "Ask pi (selection)" })
