6a105926bccabe9014406f4b089df499ef52ea43
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2026-07-10 20:57:30 +0200

    feat(hypr): add smart gaps

--- config/hypr/core/rules.lua
+++ config/hypr/core/rules.lua
@@ -37,3 +37,11 @@ hl.layer_rule({
   match = { namespace = "selection" },
   no_anim = true,
 })
+
+-- Smart gaps
+hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, border_size = 0 })
+hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, rounding = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, border_size = 0 })
+hl.window_rule({ match = { float = false, workspace = "f[1]" }, rounding = 0 })
